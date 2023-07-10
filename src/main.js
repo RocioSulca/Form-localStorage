a596fbee7c373879b916ee9d851216df5e8b02d1
RocioSulca <rociosulca.z@gmail.com>
2023-07-10 15:48:13 -0500

    Refactorizando

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+const btnSubmit = document.getElementById('btn-submit');
+const inputVacio = document.getElementById('input-vacio');
+const data = document.getElementById("detail-container");
+let pruebaContainer = document.getElementById('detail-add-container');
+
 const registro = e => {
     let formData = {
         student: document.getElementById('student').value,
@@ -5,47 +10,61 @@ const registro = e => {
         checkout: document.getElementById('checkout').value,
         checkin: document.getElementById('checkin').value,
     }
-
-    localStorage.setItem('formData', JSON.stringify(formData));
-    student.value="";
-    book.value="";
-    checkout.value="";
-    checkin.value="";
+    let arrayVacio = JSON.parse(localStorage.getItem("formData")) || [];
+    arrayVacio.push(formData);
+    localStorage.setItem('formData', JSON.stringify(arrayVacio));
     getData();
-    e.preventDefault();
+    student.value = "";
+    book.value = "";
+    checkout.value = "";
+    checkin.value = "";
 }
 
 
+document.addEventListener("DOMContentLoaded", function () {
+    let datosPreCargados = JSON.parse(localStorage.getItem("formData"));
+    if (datosPreCargados) {
+        datosPreCargados.forEach(estudiante => {
+            data.innerHTML += `
+            <div class="control-prueba">
+            <label>${estudiante.student}</label>
+                <label>${estudiante.book}</label>
+                <label>${estudiante.checkout}</label>
+                <label>${estudiante.checkin}</label>
+            </div>
+        `;
+        })
+    }
+})
+
+
 function getData() {
-    let { student, book, checkout, checkin } = JSON.parse(localStorage.getItem("formData"));
-    const data = document.getElementById("detalle-desktop");
-    const dataMobile = document.getElementById("detalle-mobile");
-    data.innerHTML += `
-    <div class="control-prueba">
+
+    let student = document.getElementById('student').value;
+    let book = document.getElementById('book').value;
+    let checkout = document.getElementById('checkout').value;
+    let checkin = document.getElementById('checkin').value;
+
+    pruebaContainer.innerHTML += `
+        <div class="control-prueba">
         <label>${student}</label>
         <label>${book}</label>
         <label>${checkout}</label>
         <label>${checkin}</label>
     </div>
     `;
-    dataMobile.innerHTML += `
-    <div class="card">
-        <div>
-            <label class="span-label">Nombre:</label>
-            <span>${student}</span>
-        </div>
-        <div>
-            <label class="span-label">Libro:</label>
-            <span>${book}</span>
-        </div>
-        <div>
-            <label class="span-label">Fecha entrega:</label>
-            <span>${checkout}:</span>
-        </div>
-        <div>
-            <label class="span-label">Fecha salida:</label>
-            <span>${checkin}:</span>
-        </div>
-    </div>`;
 }
+
+
+btnSubmit.addEventListener('click', function (e) {
+    if (student.value.length == 0 || book.value.length == 0 || checkout.value.length == 0 || checkin.value.length == 0) {
+        inputVacio.innerHTML = `
+        <div>Debes rellenar todos los campos</div>
+        `
+    } else {
+        inputVacio.innerHTML = '';
+        registro();
+    }
+    e.preventDefault();
+})
 
